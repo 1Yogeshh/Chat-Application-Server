5bb38c6ba450e74076a630dad6433765e27d390b
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2025-12-22 23:33:20 +0530

    add

---
+++ AuthService/prisma/migrations/20251222180139_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."AuthUser" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuthUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AuthUser_email_key" ON "public"."AuthUser"("email");
--- AuthService/src/server.js
+++ AuthService/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const prisma = require("./prisma")
 
 const app = express()
 const port = 5000
@@ -6,3 +7,17 @@ const port = 5000
 app.listen(port,()=>{
     console.log(`server start successfully! ${port}`)
 })
+
+app.get("/db-check", async (req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log("hello world!")
+    res.json({ status: "SUCCESS", message: "DB connected ✅" });
+  } catch (err) {
+    res.status(500).json({
+      status: "FAILED",
+      message: "DB connection failed ❌",
+      error: err.message,
+    });
+  }
+});
