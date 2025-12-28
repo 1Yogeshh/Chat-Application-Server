3dd0115f70a82abe4c49ab0c7cd93e2ad055f178
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2025-12-28 21:53:45 +0530

    db connect in user service

--- UserService/src/server.js
+++ UserService/src/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const prisma = require("./prisma")
 
 const app = express()
+const port = 5001
 
-app.listen(()=>{
-    console.log("user service start....")
+app.listen(port, ()=>{
+    console.log(`user service start.... ${port}`)
+})
+
+app.get("/db-check", async(req, res)=>{
+    try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log("hello world!")
+    res.json({ status: "SUCCESS", message: "DB connected ✅" });
+  } catch (err) {
+    res.status(500).json({
+      status: "FAILED",
+      message: "DB connection failed ❌",
+      error: err.message,
+    });
+  }
 })
