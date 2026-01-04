0c0be9987bd4ea6e2a3f5841c9a394246517f0d0
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-04 20:00:04 +0530

    add

--- ChatService/src/server.js
+++ ChatService/src/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express();
 const port = 5002
 
+//check server start
 app.listen(port,()=>{
     console.log(`server start at post number ${port}`)
 })
