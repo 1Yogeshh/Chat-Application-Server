50f979dba966c97a8e15185a17c775f1d7b51c08
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-14 23:00:48 +0530

    redis url add

--- ChatService/src/config/redis.js
+++ ChatService/src/config/redis.js
@@ -1,39 +1,39 @@
-// const { createClient } = require("redis")
+const { createClient } = require("redis")
 
-// const redisUrl = process.env.REDIS_URL
+const redisUrl = process.env.REDIS_URL
 
-// //publisher
-// const redisPublisher = createClient({
-//     url: redisUrl
-// })
+//publisher
+const redisPublisher = createClient({
+    url: redisUrl
+})
 
-// //subscribe
-// const redisSubscriber = createClient({
-//     url: redisUrl
-// })
+//subscribe
+const redisSubscriber = createClient({
+    url: redisUrl
+})
 
-// redisPublisher.on("connect", () => {
-//   console.log("Redis Publisher Connected");
-// });
+redisPublisher.on("connect", () => {
+  console.log("Redis Publisher Connected");
+});
 
-// redisSubscriber.on("connect", () => {
-//   console.log("Redis Subscriber Connected");
-// });
+redisSubscriber.on("connect", () => {
+  console.log("Redis Subscriber Connected");
+});
 
-// redisPublisher.on("error", (err) => {
-//   console.error("Redis Publisher Error:", err);
-// });
+redisPublisher.on("error", (err) => {
+  console.error("Redis Publisher Error:", err);
+});
 
-// redisSubscriber.on("error", (err) => {
-//   console.error("Redis Subscriber Error:", err);
-// });
+redisSubscriber.on("error", (err) => {
+  console.error("Redis Subscriber Error:", err);
+});
 
-// (async ()=>{
-//     await redisPublisher.connect();
-//     await redisSubscriber.connect();
-// })();
+(async ()=>{
+    await redisPublisher.connect();
+    await redisSubscriber.connect();
+})();
 
-// module.exports = {
-//     redisPublisher,
-//     redisSubscriber
-// }
+module.exports = {
+    redisPublisher,
+    redisSubscriber
+}
