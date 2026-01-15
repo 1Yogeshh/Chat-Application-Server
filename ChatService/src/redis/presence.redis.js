604d87a17fd40ecff96ca81dfe6e2d0a5c9c86b6
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-15 22:08:22 +0530

    add user online in redis presence

---
+++ ChatService/src/redis/presence.redis.js
@@ -0,0 +1,6 @@
+const {redisPublisher} = require("../config/redis")
+
+exports.userOnline= async(authUserId, socketId)=>{
+    await redisPublisher.sAdd(`user:${authUserId}:sockets`, socketId);
+    await redisPublisher.set(`user:${authUserId}:online`, "true")
+}
