36dde8504fa086cf850b1798b93d3421aa9da65f
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-16 20:21:48 +0530

    parsing the auth id and socket id in offline user service using socket handler

--- ChatService/src/socket/socket.handlers.js
+++ ChatService/src/socket/socket.handlers.js
@@ -1,9 +1,13 @@
-const {redisPublisher} = require("../config/redis")
+const { redisPublisher } = require("../config/redis")
 const presence = require("../redis/presence.redis")
 
-module.exports=(socket)=>{
+module.exports = (socket) => {
     const authUserId = socket.user.authUserId
-    console.log("hello world! : ",authUserId)
+    console.log("hello world! : ", authUserId)
 
     presence.userOnline(authUserId, socket.id)
+
+    socket.on("disconnect", () =>
+        presence.userOffline(authUserId, socket.id)
+    )
 }
