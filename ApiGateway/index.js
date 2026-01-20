9fef9d2278d538a80935c25f853fbdecf8300031
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-20 21:59:32 +0530

    add all services in api gateway

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -10,6 +10,36 @@ app.get("/health", (_, res) => {
     res.json({ status: "API Gateway running" });
 });
 
+// AUTH SERVICE
+app.use(
+    "/api/auth",
+    createProxyMiddleware({
+        target: process.env.AUTH_SERVICE,
+        changeOrigin: true,
+        pathRewrite: { "^/api/auth": "" }
+    })
+);
+
+// USER SERVICE
+app.use(
+    "/api/users",
+    createProxyMiddleware({
+        target: process.env.USER_SERVICE,
+        changeOrigin: true,
+        pathRewrite: { "^/api/users": "" }
+    })
+);
+
+// CHAT SERVICE (REST only)
+app.use(
+    "/api/chat",
+    createProxyMiddleware({
+        target: process.env.CHAT_SERVICE,
+        changeOrigin: true,
+        pathRewrite: { "^/api/chat": "" }
+    })
+);
+
 app.listen(process.env.PORT, () => {
-  console.log(`🚪 Simple API Gateway running on port ${process.env.PORT}`);
+    console.log(`🚪 Simple API Gateway running on port ${process.env.PORT}`);
 });
