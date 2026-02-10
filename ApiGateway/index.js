7bcf7c39b031c41bd8c77764d52ce8976c8220e3
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-10 23:42:17 +0530

    add

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -6,7 +6,7 @@ const { getComposedChats } = require("./chat.compose");
 const { protect } = require("./middlware/Middleware")
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 app.use(cors({
     origin: "http://localhost:5173",
