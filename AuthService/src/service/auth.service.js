c4c2b06e39eee8735f031a6652cb0e39b30e3666
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-26 22:49:24 +0530

    changes in auth service

--- AuthService/src/service/auth.service.js
+++ AuthService/src/service/auth.service.js
@@ -1,68 +1,88 @@
+const authRepository = require("../repositories/auth.repository")
+const { hashPassword, comparePassword } = require("../utils/password.util")
 const prisma = require("../prisma")
 const bcrypt = require("bcrypt")
-const {generateAccessToken, generateRefreshToken} = require("../config/generateToken")
+const { generateAccessToken, generateRefreshToken } = require("../config/generateToken")
 const hashToken = require("../utils/hashToken")
 const redis = require("../config/redis")
 
-const register = async(email, password)=>{
-    const existUser = await prisma.authUser.findUnique({
-        where:{email}
-    })
+const register = async (email, password) => {
+    // const existUser = await prisma.authUser.findUnique({
+    //     where:{email}
+    // })
 
-    if(existUser){
+    const existUser = await authRepository.findUserByEmail(email)
+
+    if (existUser) {
         throw new Error("user already registered")
     }
 
-    const hashPassword = await bcrypt.hash(password, 10)
+    // const hashPassword = await bcrypt.hash(password, 10)
+    const hashed = await hashPassword(password)
 
-    const user = await prisma.authUser.create({
-        data:{
-            email,
-            password:hashPassword,
-        }
+    // const user = await prisma.authUser.create({
+    //     data: {
+    //         email,
+    //         password: hashPassword,
+    //     }
+    // })
+
+    const user = await authRepository.createUser({
+        email,
+        password: hashed
     })
 
     delete user.password;
     return user;
 }
 
-const getAllUsers = async()=>{
-    return prisma.authUser.findMany({
-        select:{
-            id:true,
-            email:true,
-            createdAt:true
-        }
-    })
+const getAllUsers = async () => {
+    // return prisma.authUser.findMany({
+    //     select: {
+    //         id: true,
+    //         email: true,
+    //         createdAt: true
+    //     }
+    // })
+    return authRepository.getAllUsers();
 }
 
-const login = async(email, password)=>{
-    
-    const user = await prisma.authUser.findUnique({
-        where:{email}
-    })
+const login = async (email, password) => {
 
-    if(!user){
+    // const user = await prisma.authUser.findUnique({
+    //     where: { email }
+    // })
+
+    const user = await authRepository.findUserByEmail(email)
+
+    if (!user) {
         throw new Error("Invalid Credentials")
     }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+    // const isMatch = await bcrypt.compare(password, user.password)
+    const isMatch = await comparePassword(password, user.password)
 
-    if(!isMatch){
+    if (!isMatch) {
         throw new Error("Invalid Credentials")
     }
-    
+
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user)
 
     const refreshTokenHash = hashToken(refreshToken)
 
-    await prisma.refreshToken.create({
-        data:{
-            userId: user.id,
-            tokenHash: refreshTokenHash,
-            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
-        }
+    // await prisma.refreshToken.create({
+    //     data: {
+    //         userId: user.id,
+    //         tokenHash: refreshTokenHash,
+    //         expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+    //     }
+    // })
+
+    await authRepository.createRefreshToken({
+        userId: user.id,
+        tokenHash: refreshTokenHash,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     })
 
     await redis.set(
@@ -74,7 +94,7 @@ const login = async(email, password)=>{
 
     const { password: _, ...safeUser } = user;
 
-    return {safeUser, accessToken, refreshToken};
+    return { safeUser, accessToken, refreshToken };
 }
 
-module.exports= {register, getAllUsers, login}
+module.exports = { register, getAllUsers, login }
