f24f45e4aaaca5443a9b5fc91a37d614d2afbd87
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-03-06 19:39:47 +0530

    add

--- AuthService/src/routes/auth.routes.js
+++ AuthService/src/routes/auth.routes.js
@@ -12,13 +12,6 @@ router.get("/users", authController.getAllUsers)
 
 router.post("/login", validate(loginSchema), authController.login)
 
-router.get("/me", protect, (req, res) => {
-    res.json({
-        message: "User profile",
-        user: req.user,
-    })
-})
-
 router.post("/refresh-token", authController.refreshToken)
 
 router.post("/logout", authController.logout)
