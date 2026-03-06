e4c582649688fc40e0d0ecd26fc6ef0994106784
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-03-06 19:38:59 +0530

    add

--- AuthService/src/middlewares/validate.middleware.js
+++ AuthService/src/middlewares/validate.middleware.js
@@ -1,14 +1,9 @@
 const validate = (schema, source = "body") => (req, res, next) => {
     try {
-
         const data = schema.parse(req[source])
-
         req[source] = data
-
         next()
-
     } catch (err) {
-
         return res.status(400).json({
             success: false,
             errors: err.flatten()
